12c66bb3dd5388b774f6aff2dbdf6fbd81557aec
pauliescanlon <pauliescanlon@gmail.com>
2020-03-17 11:30:00 +0000

    remove dotenv and test build

--- functions/apollo-graphql/apollo-graphql.js
+++ functions/apollo-graphql/apollo-graphql.js
@@ -1,5 +1,5 @@
 // require("dotenv").config()
-if (process.env.NODE_ENV !== "production") require("dotenv").config()
+// if (process.env.NODE_ENV !== "production") require("dotenv").config()
 const faunadb = require("faunadb")
 const { ApolloServer, gql } = require("apollo-server-lambda")
 
