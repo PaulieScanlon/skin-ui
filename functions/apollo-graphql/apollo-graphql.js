9565cd383172b3a6c29966e30fdbb12e40efac0a
pauliescanlon <pauliescanlon@gmail.com>
2020-03-18 10:13:45 +0000

    add cors to apollo server lambda

--- functions/apollo-graphql/apollo-graphql.js
+++ functions/apollo-graphql/apollo-graphql.js
@@ -106,4 +106,9 @@ const server = new ApolloServer({
   introspection: true,
 })
 
-exports.handler = server.createHandler()
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+})
